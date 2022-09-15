caed3ff7d701a553231359605058681029662a08
Pushp Singh <pushp_singh@condenast.com>
2022-09-15 20:33:17 +0530

    Updates

--- src/component/drop-down/mock-data.js
+++
@@ -1,6 +0,0 @@
-export const mockdata = [
-    {id: 0, name: 'Select Filter', value: ''},
-    {id: 1, name: 'Name', value: 'name'},
-    {id: 2, name: 'Email', value: 'email'},
-    {id: 3, name: 'Role', value: 'role'},
-];
